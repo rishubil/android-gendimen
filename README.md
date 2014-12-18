2550bd675360a100e608c6520847500d68a64040
rishubil <rishubil@gmail.com>
2014-12-18 16:33:50 +0900

    reinit

--- README.md
+++ README.md
@@ -1,4 +1,121 @@
 android-gendimen
 ================
 
-A simple tool for android dimens.xml
+Android의 dimen.xml 작성을 도와주는 간단한 도구입니다.
+
+## Quick start
+
+
+### 1. Writing dimens.xml
+
+``` xml
+<resources>
+    <dimen name="margin_small">8dp</dimen>
+
+    <!-- two times of small margin {{ margin_small * 2 }} -->
+    <dimen name="margin_large">0dp</dimen>
+    
+    <!-- {{ margin_largest = margin_small * 3 }} -->
+    <dimen name="margin_largest">0dp</dimen>
+    
+    <!-- {{ ( margin_small + margin_largest ) / 2 }} -->
+    <dimen name="margin_mean">0dp</dimen>
+    
+    <!-- {{ ( margin_small ** 2 ) / 3.0 }} -->
+    <dimen name="complex_dimen">0dp</dimen>
+
+</resources>
+```
+
+### 2. Run gendimen.py
+
+	$ python gendimen.py <filepath of dimens.xml>
+	
+### 3. Check generated dimens
+
+``` xml
+<resources>
+    <dimen name="margin_small">8dp</dimen>
+
+    <!-- two times of small margin {{ margin_small * 2 }} -->
+    <dimen name="margin_large">16dp</dimen>
+    
+    <!-- {{ margin_largest = margin_small * 3 }} -->
+    <dimen name="margin_largest">24dp</dimen>
+    
+    <!-- {{ ( margin_small + margin_largest ) / 2 }} -->
+    <dimen name="margin_mean">12dp</dimen>
+    
+    <!-- {{ ( margin_small ** 2 ) / 3.0 }} -->
+    <dimen name="complex_dimen">21.333333333333332dp</dimen>
+
+</resources>
+```
+
+	
+## Usage
+기본적인 사용법은 주석 사이에 ```{{ expr }}``` 와 같은 표현식을 삽입하여 동작합니다.
+
+``` xml
+<dimen name="margin_small">8dp</dimen>
+
+<!-- {{ margin_large = margin_small * 2 }} -->
+<dimen name="margin_large">0dp</dimen>
+```
+
+<!-- {{ margin_large = margin_small * 2 }} -->
+<dimen name="margin_large">0dp</dimen>
+```
+
+만약 수정할 dimen tag의 바로 위에 위치한 주석에 expr을 사용하는 경우라면, 좌측 표현식과 과 등호를 생략하고 사용할 수 있습니다.
+
+``` xml
+<dimen name="margin_small">8dp</dimen>
+
+<!-- {{ margin_small * 2 }} -->
+<dimen name="margin_large">0dp</dimen>
+```
+
+반대로, 좌측 표현식과 등호를 포함한다면 반드시 expr이 해당 dimen tag의 바로 위가 아닌 문서의 어느 위치에 존재해도 괜찮습니다.
+
+``` xml
+<!-- {{ margin_large = margin_small * 2 }} -->
+<dimen name="margin_small">8dp</dimen>
+<dimen name="margin_large">0dp</dimen>
+```
+
+#### 주의사항
+* 서로 다른 단위끼리는 연산할 수 없습니다.
+	``` xml
+	<dimen name="text_size">24sp</dimen>
+	<dimen name="padding">8dp</dimen>
+
+	<!-- {{ new_dimen = text_size + padding }} : ERROR! -->
+	<dimen name="new_dimen">8dp</dimen>
+	```
+
+* 무한 루프를 야기할 수 있는 상호참조는 사용할 수 없습니다.
+	``` xml
+	<!-- {{ foo = bar + fuz }} : ERROR! -->
+	<dimen name="foo">4dp</dimen>
+	<!-- {{ bar = fuz }} : ERROR! -->
+	<dimen name="bar">8dp</dimen>
+	<!-- {{ fuz = foo }} : ERROR! -->
+	<dimen name="fuz">6dp</dimen>
+	```
+
+* 한 주석에서는 가장 첫 expr만 연산되며, 나머지는 평가되지 않습니다.
+	``` xml
+	<dimen name="margin_small">8dp</dimen>
+
+	<!-- {{ margin_small * 2 }}  {{ it = doesn't + matter }} -->
+	<dimen name="margin_large">0dp</dimen>
+	```
+
+* 계산된 결과의 단위는 기존 dimen tag에 사용하던 단위와 같으며, 아직 계산하지 않은 dimen tag라도 미리 사용할 단위를 명시해놓아야 합니다.
+	``` xml
+	<dimen name="margin_small">8dp</dimen>
+
+	<!-- {{ margin_small * 2 }}
+	<dimen name="margin_large">0</dimen> <!-- ERROR! -->
+	```
---
+++ gendimen.py
@@ -0,0 +1,187 @@
+from lxml import etree as ET
+import re
+import copy
+from pprint import pprint
+import sys
+import argparse
+from pygraph.classes.digraph import digraph
+from pygraph.algorithms.cycles import find_cycle
+
+
+def parseUnit(string):
+    for unit in ('px', 'in', 'mm', 'pt', 'dp', 'sp'):
+        regex = re.compile("(^|[\s\W\.])(\d*\.?\d*)(%s)" % unit)
+        s = regex.search(string)
+        if s:
+            result = {}
+            result['size'] = s.group(2)
+            result['unit'] = s.group(3)
+
+            if '.' in result['size']:
+                result['size'] = float(result['size'])
+            else:
+                result['size'] = int(result['size'])
+
+            return result
+
+
+def mergeUnit(unit):
+    return str(unit['size']) + unit['unit']
+
+
+def getXml(filename):
+    dimen_xml = open(filename, 'r')
+
+    parser = ET.XMLParser(remove_comments=False)
+    e = ET.fromstring(dimen_xml.read(), parser=parser)
+    dimen_xml.close()
+    return e
+
+
+def saveXml(e, filename):
+    new_xml = ET.tostring(e, pretty_print=True)
+    dimen_xml = open(filename, 'w')
+    dimen_xml.write(new_xml)
+    dimen_xml.close()
+
+
+def parseDimens(e, dimens):
+    for dimen in e.findall(".dimen"):
+        size = parseUnit(dimen.text)
+        dimens[dimen.attrib['name']] = size
+
+
+def setDimens(e, dimens):
+    for dimen in e.findall(".dimen"):
+        dimen.text = dimens[dimen.attrib['name']]
+
+
+def getChanges(dimens, ori_dimens):
+    changes = []
+    for k in dimens.keys():
+        if dimens[k] != ori_dimens[k]:
+            changes.append(" * %s : %s -> %s" % (k, ori_dimens[k], dimens[k]))
+    return changes
+
+
+def confirm(changes, yes):
+    if changes:
+        print("[-] Following dimens will be changed.")
+        print("\n")
+        for s in changes:
+            print(s)
+        print("\n")
+
+        if not yes:
+            isOK = raw_input("[=] Procced?[y/N]: ")
+            if not isOK in ('Y', 'y'):
+                sys.stderr.write("[-] Cancled.")
+                sys.exit(-2)
+    else:
+        print("[-] Nothing to change.")
+        sys.exit(0)
+
+
+def getExprFromComment(element):
+    if not element.tag is ET.Comment:
+        return None
+    comment = element.text
+    regex = re.compile("{{(.*)}}")
+
+    s = regex.search(comment)
+    if not s:
+        return None
+    expr = s.group(1).strip()
+    if "{{" in expr or "}}" in expr:
+        sys.stderr.write("[!] Abnormal Expression: %s" % comment)
+        sys.exit(-1)
+    return expr
+
+
+def addEdge(gp, expr):
+    (left_expr, right_expr) = expr.split('=')
+    lefts = []
+    rights = []
+
+    for dimen in dimens.keys():
+        if dimen in left_expr:
+            lefts.append(dimen)
+        if dimen in right_expr:
+            rights.append(dimen)
+
+    for left in lefts:
+        for right in rights:
+            gp.add_edge((left, right))
+
+
+def run(dimens):
+    expr = None
+    gp = digraph()
+    gp.add_nodes(dimens.keys())
+
+    for element in e.iter():
+        if expr:
+            if element.tag is ET.Comment:
+                continue
+            if not "=" in expr:
+                left = element.attrib['name']
+                expr = "%s = %s" % (left, expr)
+
+            addEdge(gp, expr)
+
+            runExpr(expr, dimens)
+            expr = None
+        else:
+            expr = getExprFromComment(element)
+            if not expr:
+                continue
+
+    if '__builtins__' in dimens:
+        del dimens['__builtins__']
+
+    cycles = find_cycle(gp)
+    if cycles:
+        sys.stderr.write("[!] Exist dependency cycle: %s" % (cycles))
+        sys.exit(-1)
+
+
+def runExpr(expr, dimens):
+    unit = None
+    for dimen in dimens.keys():
+        if dimen in expr:
+            if unit and unit != dimens[dimen]['unit']:
+                sys.stderr.write("[!] Unit mismatched: %s" % expr)
+                sys.exit(-1)
+            unit = dimens[dimen]['unit']
+            expr = expr.replace(dimen, "%s['size']" % dimen)
+    exec expr in dimens
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filename", help="Filename of dimens.xml")
+    parser.add_argument("-y", "--yes", help="Reply 'Y' for all interactions", action="store_true")
+    args = parser.parse_args()
+
+    e = getXml(args.filename)
+
+    dimens = {}
+    parseDimens(e, dimens)
+
+    ori_dimens = copy.deepcopy(dimens)
+
+    run(dimens)
+
+    for k in dimens.keys():
+        dimens[k] = mergeUnit(dimens[k])
+        ori_dimens[k] = mergeUnit(ori_dimens[k])
+
+    changes = getChanges(dimens, ori_dimens)
+
+    confirm(changes, args.yes)
+
+    setDimens(e, dimens)
+    saveXml(e, args.filename)
+
+    print("[-] Complete.")
+    sys.exit(0)
